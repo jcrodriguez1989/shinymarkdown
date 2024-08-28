e437051fd52f2f18255ca1c706b3b4ff024328d0
jcrodriguez1989 <jcrodriguez@unc.edu.ar>
2024-08-28 08:37:29 -0300

    Styling

--- inst/assets/js/init-template.js
+++ inst/assets/js/init-template.js
@@ -1,19 +1,19 @@
 $(document).ready( function () {
 
-const Editor = toastui.Editor;
-const {{inputId}}_editor = new Editor({
-  el: document.querySelector('#' + '{{inputId}}' + '_editor'),
-  usageStatistics: false, /* this will not be customizable. */
-  minHeight: '{{min_height}}',
-  height: '{{height}}',
-  previewStyle: '{{preview_style}}',
-  previewHighlight: {{preview_highlight}},
-  initialEditType: '{{initial_edit_type}}',
-  hideModeSwitch: {{hide_mode_switch}},
-  language: '{{language}}'{{#initial_value_lgl}},
-  initialValue: `{{initial_value}}`,
-  {{ /initial_value_lgl }}
-});
+  const Editor = toastui.Editor;
+  const {{inputId}}_editor = new Editor({
+    el: document.querySelector('#' + '{{inputId}}' + '_editor'),
+    usageStatistics: false, /* this will not be customizable. */
+    minHeight: '{{min_height}}',
+    height: '{{height}}',
+    previewStyle: '{{preview_style}}',
+    previewHighlight: {{preview_highlight}},
+    initialEditType: '{{initial_edit_type}}',
+    hideModeSwitch: {{hide_mode_switch}},
+    language: '{{language}}'{{#initial_value_lgl}},
+    initialValue: `{{initial_value}}`,
+    {{ /initial_value_lgl }}
+  });
 
   /* When someone types in the editor, make the markdown and HTML available
      as the Shiny/R inputs "shinymd_markdown" and "shinymd_html", respectively. */
--- inst/assets/js/shinymarkdown-handlers.js
+++ inst/assets/js/shinymarkdown-handlers.js
@@ -1,9 +1,14 @@
 // hide the editor when Shiny sends the message "hide_editor"
-  Shiny.addCustomMessageHandler("hide_editor", function (params) {
-    $('#' + params.id + '_editor').hide();
-  });
+Shiny.addCustomMessageHandler("hide_editor", function (params) {
+  $('#' + params.id + '_editor').hide();
+});
 
-  // show the editor when Shiny sends the message "show_editor"
-  Shiny.addCustomMessageHandler("show_editor", function (params) {
-    $('#' + params.id + '_editor').show();
-  });
+// show the editor when Shiny sends the message "show_editor"
+Shiny.addCustomMessageHandler("show_editor", function (params) {
+  $('#' + params.id + '_editor').show();
+});
+
+// insert text when Shiny sends the message "insertText"
+Shiny.addCustomMessageHandler("insert_text", function (params) {
+  $('#' + params.id + '_editor').insertText(params.text);
+});
